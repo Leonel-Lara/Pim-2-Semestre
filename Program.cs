4686338e0f4238ef7da433c9d002515bb57db855
Ana <anamassaro02@gmail.com>
2020-10-16 13:31:23 -0300

    reserva de assentos

--- Program.cs
+++ Program.cs
@@ -6,21 +6,26 @@ namespace pim_2semestre
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Bem vindo ao nosso evento, é um prazer tê-lo conosco!");
 
-      int seat = 1;
+      //int seat = 1;
+
+      int linha, coluna;
+      int[] col = new int[2];
+      int[] lin = new int[2];
 
       bool loop = true;
       while (loop)
       {
-        int i = seat++;
+        //int i = seat++;
+
+        Console.WriteLine("Bem vindo ao nosso evento, é um prazer tê-lo conosco!");
 
         int option = 0;
         Console.WriteLine("Digite '1' para se cadastrar! '2' para sair");
         option = Convert.ToInt32(Console.ReadLine());
 
         string date = DateTime.Now.ToString("dd-MM-yyyy");
-
+       
         switch (option)
         {
           case 1:
@@ -31,20 +36,58 @@ namespace pim_2semestre
             Console.WriteLine("Qual seu nome?");
             name = Console.ReadLine();
 
-            if (i > 5)
+            Console.WriteLine("Escolha primeiramente a linha de seu assento:");
+            linha = Convert.ToInt32(Console.ReadLine());
+
+
+            if (lin[linha] == 1)
             {
-              Console.WriteLine("Todos os assentos estão reservados!");
+              Console.WriteLine("Assento ocupado!");
             }
             else
             {
-              Console.WriteLine("Nome: " + name + "\r\nAssento: " + i + "\r\nRegistrado em: " + date);
-            }
+              for (int k = 1; k < 2; k++)
+              {
+                if (linha == k)
+                {
+                  lin[k] = 1;
+                }
+              }
 
+            Console.WriteLine("Escolha coluna");
+            coluna = Convert.ToInt32(Console.ReadLine());
+
+            for (int j = 1; j < 2; j++)
+            {
+              if (col[coluna] == 1)
+              {
+                Console.WriteLine("Assento ocupado!");
+              }
+              else
+              {
+                if (coluna == j)
+                {
+                  col[j] = 1;
+                  
+                  Console.WriteLine("Nome: " + name + "\r\nColuna: " + coluna + "\r\nLinha:" + linha +"\r\nRegistrado em: " + date);
+                }
+
+              }
+            }
+          }
+           
             break;
 
           case 2:
             Console.WriteLine("Saindo do sistema!");
             loop = false;
+            break;
+
+          case 3:
+
+            break;
+          default:
+            Console.WriteLine("Tecla inválida");
             break;
         }
       }
